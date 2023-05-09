53f5bd87e68a530318967a4aad9ce26d476cfdcb
unknown <chandolkarshreyas001@gmail.com>
2023-05-09 20:10:27 +0530

    update

--- wallstreet/wallstreet/settings.py
+++ wallstreet/wallstreet/settings.py
@@ -59,7 +59,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://wallstreet.credenz.in/',
+]
 
 ROOT_URLCONF = 'wallstreet.urls'
 
